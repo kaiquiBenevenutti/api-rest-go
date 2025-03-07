6d2ab67ecf638aabe46d9576bf2a0392d7e66908
Kaiqui Benevenutti <benevenuttikaiqui@gmail.com>
2025-03-07 08:51:30 -0300

    json

--- api-rest-go/main.go
+++ api-rest-go/main.go
@@ -1,11 +1,18 @@
 ﻿package main
 
 import (
+	"api-rest-go/models"
 	"api-rest-go/routes"
 	"fmt"
 )
 
 func main() {
+	models.Personalidades = []models.Personalidade{
+		{Nome: "Nome 1", Historia: "Historia 1"},
+		{Nome: "Nome 2", Historia: "Historia 2"},
+		{Nome: "Nome 3", Historia: "Historia 3"},
+	}
+
 	fmt.Println("Iniciando servidor..")
 	routes.HandleRequest()
 }
---
+++ api-rest-go/models/personalidade.go
@@ -0,0 +1,8 @@
+﻿package models
+
+type Personalidade struct {
+	Nome     string `json:"nome"`
+	Historia string `json:"historia"`
+}
+
+var Personalidades = []Personalidade{}
