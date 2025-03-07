395481bdd27bab5af4eb060c77b687e557e8f5e9
Kaiqui Benevenutti <benevenuttikaiqui@gmail.com>
2025-03-07 08:21:51 -0300

    initial

---
+++ api-rest-go/go.mod
@@ -0,0 +1,3 @@
+module api-rest-go
+
+go 1.23
---
+++ api-rest-go/main.go
@@ -0,0 +1,21 @@
+﻿package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Home Page")
+}
+
+func HandleRequest() {
+	http.HandleFunc("/", Home)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func main() {
+	fmt.Println("Iniciando servidor..")
+	HandleRequest()
+}
